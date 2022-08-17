bdbd71d4f8edaff1b9ea9e11e53839c2e3113055
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-08-17 16:21:47 +0300

    Register 'vehicle_fleet' app in the project

--- project_root/project/settings.py
+++ project_root/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vehicle_fleet.apps.VehicleFleetConfig',
 ]
 
 MIDDLEWARE = [
--- project_root/vehicle_fleet/apps.py
+++ project_root/vehicle_fleet/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class VehicleFleetConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'vehicle_fleet'
