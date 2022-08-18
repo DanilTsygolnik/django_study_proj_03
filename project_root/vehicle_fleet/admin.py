09268db0938911e4afe21080299b739983133ff2
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-08-18 12:23:53 +0300

    Register models in the admin

--- project_root/vehicle_fleet/admin.py
+++ project_root/vehicle_fleet/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import *
 
 
+admin.site.register(VehicleBrand)
+admin.site.register(VehicleModelInBrand)
+admin.site.register(VehicleCategory)
+admin.site.register(VehicleFuel)
+admin.site.register(VehicleEngineCapacity)
+admin.site.register(VehicleTransmission)
+admin.site.register(VehicleTypeOfDrive)
+admin.site.register(VehicleNumSeats)
+admin.site.register(VehicleNumDoors)
+admin.site.register(VehicleBodyColor)
 admin.site.register(Vehicle)
-
