ab1adbf67e4506c8d36a495cfc5a5b9abfad8ac7
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-08-18 11:32:16 +0300

    Add 'VehicleModelInBrand' model

--- project_root/vehicle_fleet/models.py
+++ project_root/vehicle_fleet/models.py
@@ -6,3 +6,15 @@ class VehicleBrand(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class VehicleModelInBrand(models.Model):
+    vehicle_brand = models.ForeignKey(
+        VehicleBrand,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=20, verbose_name='Vehicle Model')
+
+    def __str__(self):
+        return self.title
+
