ae263ecd218add81ce0fec50ce394bc8366abb38
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-08-18 12:08:49 +0300

    Add 'Vehicle' model

--- project_root/vehicle_fleet/models.py
+++ project_root/vehicle_fleet/models.py
@@ -78,3 +78,74 @@ class VehicleBodyColor(models.Model):
     def __str__(self):
         return self.title
 
+
+class Vehicle(models.Model):
+    brand = models.ForeignKey(
+        VehicleBrand, 
+        on_delete=models.CASCADE
+    )
+    model_in_brand = models.ForeignKey(
+        VehicleModelInBrand, 
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        VehicleCategory, 
+        on_delete=models.PROTECT
+    )
+    price_usd = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+        verbose_name='Price, USD'
+    )
+    year_of_manufacture = models.IntegerField(verbose_name='Year Of Manufacture')
+    mileage_km = models.IntegerField(verbose_name='Mileage, km')
+    fuel = models.ForeignKey(
+        VehicleFuel, 
+        blank=True,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    engine_capacity = models.ForeignKey(
+        VehicleEngineCapacity, 
+        blank=True,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    transmission = models.ForeignKey(
+        VehicleTransmission, 
+        blank=True,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    type_of_drive = models.ForeignKey(
+        VehicleTypeOfDrive, 
+        blank=True,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    num_seats = models.ForeignKey(
+        VehicleNumSeats, 
+        blank=True,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    num_doors = models.ForeignKey(
+        VehicleNumDoors, 
+        blank=True,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    body_color = models.ForeignKey(
+        VehicleBodyColor, 
+        blank=True,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name='Vehicle Description'
+    )
+
+    def __str__(self):
+        return self.model_in_brand
+
