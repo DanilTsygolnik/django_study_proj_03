a69c3dde6488143dd2d1f9ccd6319c1ace92c512
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-08-18 11:26:59 +0300

    Add 'VehicleBrand' model

--- project_root/vehicle_fleet/models.py
+++ project_root/vehicle_fleet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 
+class VehicleBrand(models.Model):
+    title = models.CharField(max_length=20, verbose_name='Vehicle Brand')
+
+    def __str__(self):
+        return self.title
