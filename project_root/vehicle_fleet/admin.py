5aaf2f7762f8636988097ffd14c7a6ef3c02eab6
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-08-18 14:01:38 +0300

    Show extra columns for 'Vehicle' model in the admin

--- project_root/vehicle_fleet/admin.py
+++ project_root/vehicle_fleet/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 from .models import *
 
 
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'model_in_brand',
+        'category',
+        'year_of_manufacture',
+        'mileage_km',
+        'transmission',
+        'fuel',
+        'price_usd'
+    )
+
+
 admin.site.register(VehicleBrand)
 admin.site.register(VehicleModelInBrand)
 admin.site.register(VehicleCategory)
@@ -12,4 +25,4 @@ admin.site.register(VehicleTypeOfDrive)
 admin.site.register(VehicleNumSeats)
 admin.site.register(VehicleNumDoors)
 admin.site.register(VehicleBodyColor)
-admin.site.register(Vehicle)
+admin.site.register(Vehicle, VehicleAdmin)
