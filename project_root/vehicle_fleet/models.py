e1c0bd7c8216394ae3e0eb1c9b3b598398756ec8
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-08-18 11:18:43 +0300

    Begin rewriting 'Vehicle' model

--- project_root/vehicle_fleet/models.py
+++ project_root/vehicle_fleet/models.py
@@ -1,22 +1,3 @@
 from django.db import models
 
 
-class Vehicle(models.Model):
-    price_usd = models.DecimalField(
-        max_digits=12,
-        decimal_places=2,
-        verbose_name='Price, USD'
-    )
-    year_of_manufacture = models.IntegerField(verbose_name='Year Of Manufacture')
-    mileage_km = models.IntegerField(verbose_name='Mileage, km')
-    engine_capacity_liters = models.DecimalField(
-        max_digits=3,
-        decimal_places=1,
-        verbose_name='Engine Capacity, liters'
-    )
-    num_seats = models.IntegerField(verbose_name='Seats In Vehicle')
-    num_doors = models.IntegerField(verbose_name='Doors In Vehicle')
-    description = models.TextField(
-        blank=True,
-        verbose_name='Vehicle Description'
-    )
