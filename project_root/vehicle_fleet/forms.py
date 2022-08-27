6549874776dd44705bcb3a79cfe136927b6f9b78
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-08-27 07:25:07 +0300

    Prevent reassigning the owner of a busy vehicle (wip, fail)
    Add 'Vehicle' model to get owner_id value stored in DB

--- project_root/vehicle_fleet/forms.py
+++ project_root/vehicle_fleet/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 
+from .models import Vehicle
+
 
 class VehicleAdminForm(forms.ModelForm):
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
+
     def clean_owner_id(self):
         field_id = 'owner_id'
         value_form_cleaned = self.cleaned_data[field_id]
-        value_database = self.__dict__[field_id]
+        vehicle_id = self.cleaned_data['id']
+        value_database = Vehicle.objects.get(id=vehicle_id).__dict__[field_id]
         user_changed_field = (value_form_cleaned != value_database)
         num_vehicle_active_drivers = len(self.driver_set.filter(is_driving=True))
         vehicle_is_busy = (num_vehicle_active_drivers != 0)
