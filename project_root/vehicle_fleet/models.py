85acf3267aa1ac6f7d32a17081d72d37201cfe46
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-08-29 18:07:57 +0300

    Add 'Manager' model

--- project_root/vehicle_fleet/models.py
+++ project_root/vehicle_fleet/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Manager(AbstractUser):
+    pass # For now we do nothinng
+
+    def __str__(self):
+        return self.username
