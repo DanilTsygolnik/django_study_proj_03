928cf339d49528ede6afb9b1d194d73ffcdf180c
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-08-29 18:21:04 +0300

    Resolve ImportError

--- project_root/vehicle_fleet/admin.py
+++ project_root/vehicle_fleet/admin.py
@@ -1,34 +1,3 @@
 from django.contrib import admin
-from .models import *
-from .forms import VehicleAdminForm
 
-class DriverAdmin(admin.ModelAdmin):
-    list_display = (
-    'id',
-    'first_name',
-    'last_name',
-    'num_years_experience',
-    'employer',
-    'is_driving'
-    )
-
-
-class VehicleAdmin(admin.ModelAdmin):
-    form = VehicleAdminForm
-    list_display = (
-        'id',
-        'brand',
-        'model',
-        'license_plate_num',
-        'vehicle_type',
-        'year_of_manufacture',
-        'mileage_km',
-        'price_usd',
-    )
-    list_display_links = ('id',)
-
-
-admin.site.register(Driver, DriverAdmin)
-admin.site.register(Enterprise)
-admin.site.register(VehicleBrand)
-admin.site.register(Vehicle, VehicleAdmin)
+# Register your models here.
